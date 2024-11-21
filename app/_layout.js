504b4f25f0b40eddd4b5abfd62b0b30e12a411a5
darkars33 <darshangarad18@gmail.com>
2024-11-21 20:47:21 +0530

    minor update

--- app/_layout.js
+++ app/_layout.js
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useFonts } from 'expo-font';
+import {View, Text} from 'react-native';
 
 SplashScreen.preventAutoHideAsync();
 
